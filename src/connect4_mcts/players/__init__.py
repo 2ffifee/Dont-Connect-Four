27bb9e114885b4618cb5b023e880951c454779c2
Filip <filip.filipkowski6@gmail.com>
2026-05-25 15:06:03 +0200

    add minimax to cli

--- src/connect4_mcts/players/__init__.py
+++ src/connect4_mcts/players/__init__.py
@@ -1,10 +1,12 @@
 """Player implementations."""
 
 from connect4_mcts.players.base import Agent, MoveSelectionError
+from connect4_mcts.players.minimax import MinimaxPlayer
 from connect4_mcts.players.random import RandomPlayer
 
 __all__ = [
     "Agent",
+    "MinimaxPlayer",
     "MoveSelectionError",
     "RandomPlayer",
 ]
